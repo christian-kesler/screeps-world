8e032d8818734c00a95948e7cf8eda2979c3f7b0
Christian Kesler <git.ih8mp@simplelogin.com>
2023-10-22 05:23:45 -0700

    restore spawns

--- spawns.js
+++ spawns.js
@@ -1,16 +1,29 @@
+const { strategy } = require('config')
+const { creepCensus, generateSpawnDirective } = require('analytics')
+
 module.exports = {
     spawnLoop: () => {
 
         for (var name in Game.spawns) {
 
-            Game.spawns[name].spawnCreep(
-                // body
-                [MOVE, MOVE, WORK, CARRY, CARRY],
-                // name
-                `nurse_${name}_${Game.time}`,
-                // options
-                { memory: { role: 'nurse' } }
-            )
+            let census = creepCensus()
+            let directive = generateSpawnDirective(strategy, census)
+
+            console.log(`Census: ${JSON.stringify(census)}`)
+            console.log(`Directive: ${JSON.stringify(directive)}`)
+
+            for (let i = 0; i < directive.length; i++) {
+                if (directive[i].action == "spawn_creep") {
+                    Game.spawns[name].spawnCreep(
+                        // body
+                        [MOVE, MOVE, WORK, CARRY, CARRY],
+                        // name
+                        `${directive[i].role}_${name}_${Game.time}`,
+                        // options
+                        { memory: { role: directive[i].role } }
+                    )
+                }
+            }
         }
     }
 }
