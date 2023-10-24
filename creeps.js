299c46b093cc941071de152fac9a2e8d7116cc74
Christian Kesler <git.ih8mp@simplelogin.com>
2023-10-24 04:39:50 -0700

    implement target memory to avoid resource crowding with comments

--- creeps.js
+++ creeps.js
@@ -10,49 +10,58 @@ const moveToOpt = {
 
 const creepBehaviorObject = {
     nurse: (creep) => {
-        if (creep.memory.directive == 'transferResources') {
-            if (creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == -9) {
-                creep.moveTo(Game.spawns['Spawn1'], moveToOpt)
-            }
+        // deliver energy to spawn
+        if (creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == -9) {
+            creep.moveTo(Game.spawns['Spawn1'], moveToOpt)
         }
     },
     engineer: (creep) => {
-        if (creep.memory.directive == 'transferResources') {
-            if (creep.memory.transferTargetId == null) {
-                roads = creep.room.find(FIND_STRUCTURES, {
-                    filter: { structureType: STRUCTURE_ROAD }
-                })
+        if (creep.memory.useTargetId == null) {
 
-                for (var name in roads) {
-                    if (roads[name].hits <= 2000) {
-                        creep.memory.transferTargetId = roads[name].id
-                    }
-                }
+            // find roads in room
+            roads = creep.room.find(FIND_STRUCTURES, {
+                filter: { structureType: STRUCTURE_ROAD }
+            })
 
-                if (creep.memory.transferTargetId == null) {
-                    creep.memory.transferTargetId = Game.constructionSites[Object.keys(Game.constructionSites)[0]].id
+            // look for broken roads and set target if found
+            for (var name in roads) {
+                if (roads[name].hits <= 2000) {
+                    creep.memory.useTargetId = roads[name].id
                 }
+            }
 
-            } else {
-                if (Game.getObjectById(creep.memory.transferTargetId).hits == 5000) {
-                    creep.memory.transferTargetId = null
-                }
-                if (
-                    creep.build(Game.getObjectById(creep.memory.transferTargetId)) == -9
-                    ||
-                    creep.repair(Game.getObjectById(creep.memory.transferTargetId)) == -9
-                ) {
-                    console.log("attempting move")
-                    creep.moveTo(Game.getObjectById(creep.memory.transferTargetId), moveToOpt)
-                }
+            // if no broken roads found
+            if (creep.memory.useTargetId == null) {
+
+                // set target to construction site
+                creep.memory.useTargetId = Game.constructionSites[Object.keys(Game.constructionSites)[0]].id
+            }
+
+        } else {
+
+            // if target has max hits
+            if (Game.getObjectById(creep.memory.useTargetId).hits == 5000) {
+
+                // clear target and recalculate
+                creep.memory.useTargetId = null
+            }
+
+            // attempt build and repair actions
+            if (
+                creep.build(Game.getObjectById(creep.memory.useTargetId)) == -9
+                ||
+                creep.repair(Game.getObjectById(creep.memory.useTargetId)) == -9
+            ) {
+
+                // moveto target if actions failed due to distance
+                creep.moveTo(Game.getObjectById(creep.memory.useTargetId), moveToOpt)
             }
         }
     },
     upgrader: (creep) => {
-        if (creep.memory.directive == 'transferResources') {
-            if (creep.upgradeController(creep.room.controller) == -9) {
-                creep.moveTo(creep.room.controller, moveToOpt)
-            }
+        // deliver energy to controller
+        if (creep.upgradeController(creep.room.controller) == -9) {
+            creep.moveTo(creep.room.controller, moveToOpt)
         }
     },
 }
@@ -62,35 +71,47 @@ module.exports = {
 
         for (var name in Game.creeps) {
 
-            let creep = Game.creeps[name]
-            let sources = creep.room.find(FIND_SOURCES_ACTIVE)
 
-            richestEnergySource = sources[0]
-            for (let i = 0; i < sources.length; i++) {
-                if (sources[i].energy > richestEnergySource.energy) {
-                    richestEnergySource = sources[i]
+            // creep shorthand
+            let creep = Game.creeps[name]
+
+            // assessing store capacity and setting directive
+            if (creep.store.getUsedCapacity(RESOURCE_ENERGY) == 0) {
+                creep.memory.directive = 'harvestResources'
+
+                // assigning harvest target if not set 
+                if (creep.memory.harvestTargetId == null) {
+                    let sources = creep.room.find(FIND_SOURCES_ACTIVE)
+                    richestEnergySource = sources[0]
+                    for (let i = 0; i < sources.length; i++) {
+                        if (sources[i].energy > richestEnergySource.energy) {
+                            richestEnergySource = sources[i]
+                        }
+                    }
+                    creep.memory.harvestTargetId = richestEnergySource.id
+                }
+            } else if (creep.store.getFreeCapacity(RESOURCE_ENERGY) == 0) {
+                creep.memory.directive = 'useResources'
+                creep.memory.harvestTargetId = null
+            }
+
+            // performing harvest directive
+            if (creep.memory.directive == 'harvestResources') {
+                if (creep.harvest(Game.getObjectById(creep.memory.harvestTargetId)) == -9) {
+                    creep.moveTo(Game.getObjectById(creep.memory.harvestTargetId), moveToOpt)
                 }
             }
 
+            // catching unexpected directives
+            if (creep.memory.directive != 'harvestResources' && creep.memory.directive != 'useResources') {
+                creep.memory.directive = 'useResources'
+            }
+
+            // performing use directive
             try {
-                if (creep.store.getUsedCapacity(RESOURCE_ENERGY) == 0) {
-                    creep.memory.directive = 'harvestResources'
-                    creep.memory.harvestTargetId = richestEnergySource.id
-                } else if (creep.store.getFreeCapacity(RESOURCE_ENERGY) == 0) {
-                    creep.memory.directive = 'transferResources'
+                if (creep.memory.directive == 'useResources') {
+                    creepBehaviorObject[creep.memory.role](creep)
                 }
-
-                if (creep.memory.directive == 'harvestResources') {
-                    if (creep.harvest(Game.getObjectById(creep.memory.harvestTargetId)) == -9) {
-                        creep.moveTo(Game.getObjectById(creep.memory.harvestTargetId), moveToOpt)
-                    }
-                }
-
-                if (creep.memory.directive != 'harvestResources' && creep.memory.directive != 'transferResources') {
-                    creep.memory.directive = 'transferResources'
-                }
-
-                creepBehaviorObject[creep.memory.role](creep)
             } catch (err) {
                 console.log(err.message)
             }
